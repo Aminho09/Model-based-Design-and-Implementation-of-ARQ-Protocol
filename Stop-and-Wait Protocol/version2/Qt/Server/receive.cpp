fc25df41111f688ffcf24c7dadcc03d3f6ce6d50
Aminho09 <aminehsanimehr14@gmail.com>
2024-12-18 00:45:23 +0330

    bug fixed

--- Stop-and-Wait Protocol/version2/Qt/Server/receive.cpp
+++ Stop-and-Wait Protocol/version2/Qt/Server/receive.cpp
@@ -1,12 +1,11 @@
-
 //
 // File: receive.cpp
 //
 // Code generated for Simulink model 'receive'.
 //
-// Model version                  : 1.2
+// Model version                  : 1.3
 // Simulink Coder version         : 24.2 (R2024b) 21-Jun-2024
-// C/C++ source code generated on : Tue Dec 17 23:36:39 2024
+// C/C++ source code generated on : Wed Dec 18 00:42:38 2024
 //
 // Target selection: ert.tlc
 // Embedded hardware selection: Intel->x86-64 (Windows64)
@@ -15,6 +14,81 @@
 //
 #include "receive.h"
 #include <stdint.h>
+#include <stdbool.h>
+#include <cmath>
+
+extern float rt_roundf(float u);
+float rt_roundf(float u)
+{
+  float y;
+  if (std::abs(u) < 8.388608E+6F) {
+    if (u >= 0.5F) {
+      y = std::floor(u + 0.5F);
+    } else if (u > -0.5F) {
+      y = 0.0F;
+    } else {
+      y = std::ceil(u - 0.5F);
+    }
+  } else {
+    y = u;
+  }
+
+  return y;
+}
+
+// Function for Chart: '<Root>/Chart'
+uint8_t receive::receive_crc4(uint8_t b_value, float crc_tag)
+{
+  float tmp;
+  int32_t i;
+  uint16_t crc;
+  uint16_t input;
+  uint16_t tmp_0;
+  uint8_t header;
+  crc = 0U;
+  tmp = rt_roundf(crc_tag);
+  if (tmp < 65536.0F) {
+    if (tmp >= 0.0F) {
+      tmp_0 = static_cast<uint16_t>(tmp);
+    } else {
+      tmp_0 = 0U;
+    }
+  } else {
+    tmp_0 = UINT16_MAX;
+  }
+
+  input = static_cast<uint16_t>(static_cast<uint16_t>(tmp_0 << 8) | b_value);
+  for (i = 0; i < 13; i++) {
+    if (i - 8 >= 0) {
+      tmp_0 = static_cast<uint16_t>(input << static_cast<uint8_t>(-(8.0 -
+        static_cast<double>(i))));
+    } else {
+      tmp_0 = static_cast<uint16_t>(input >> static_cast<uint8_t>(8U -
+        static_cast<uint32_t>(i)));
+    }
+
+    crc = static_cast<uint16_t>(static_cast<int32_t>(tmp_0 & 1U) ^ crc);
+    if ((crc & 16U) != 0U) {
+      crc = static_cast<uint16_t>(crc ^ 19);
+    }
+
+    if (8 - i != -4) {
+      crc = static_cast<uint16_t>(crc << 1);
+    }
+  }
+
+  header = static_cast<uint8_t>(crc & 15U);
+  return header;
+}
+
+// Function for Chart: '<Root>/Chart'
+bool receive::receive_check_packet(uint16_t p, float t) const
+{
+  bool f;
+  f = ((receive_crc4(static_cast<uint8_t>(p & 255U), receive_DW.tag) == p >> 9) &&
+       (((p & 256U) != 0U) == t));
+  return f;
+}
 
 // Function for Chart: '<Root>/Chart'
 uint8_t receive::receive_ack_crc(uint8_t b_value)
@@ -50,7 +124,7 @@ void receive::receive_send_data_ack(uint16_t p, uint8_t c, uint8_t *d, uint8_t
 {
   int32_t tmp;
   int32_t tmp_0;
-  *d = static_cast<uint8_t>(p & 17U);
+  *d = static_cast<uint8_t>(p & 255U);
   if (c == 16) {
     *nc = 0U;
   } else {
@@ -80,6 +154,7 @@ void receive::receive_send_data_ack(uint16_t p, uint8_t c, uint8_t *d, uint8_t
 void receive::call()
 {
   uint8_t tmp;
+  bool out;
 
   // RootInportFunctionCallGenerator generated from: '<Root>/call' incorporates:
   //   Chart: '<Root>/Chart'
@@ -89,12 +164,16 @@ void receive::call()
   //   Outport: '<Root>/ACK'
   //   Outport: '<Root>/data'
 
-  receive_send_data_ack(receive_U.packet, receive_DW.c_ack, &receive_Y.data,
-                        &receive_Y.ACK, &tmp);
-  receive_DW.c_ack = tmp;
-  receive_DW.tag = 1.0F - receive_DW.tag;
+  out = receive_check_packet(receive_U.packet, receive_DW.tag);
+  if (out) {
+    receive_send_data_ack(receive_U.packet, receive_DW.c_ack, &receive_Y.data,
+                          &receive_Y.ACK, &tmp);
+    receive_DW.c_ack = tmp;
+    receive_DW.tag = 1.0F - receive_DW.tag;
+  }
 
-  // End of Outputs for RootInportFunctionCallGenerator generated from: '<Root>/call'
+  // End of Chart: '<Root>/Chart'
+  // End of Outputs for RootInportFunctionCallGenerator generated from: '<Root>/call' 
 }
 
 // Model initialize function
@@ -106,31 +185,13 @@ void receive::initialize()
   // SystemInitialize for Chart: '<Root>/Chart'
   receive_DW.tag = 1.0F;
 
-  // End of SystemInitialize for RootInportFunctionCallGenerator generated from: '<Root>/call'
+  // End of SystemInitialize for RootInportFunctionCallGenerator generated from: '<Root>/call' 
 }
 
 // Model terminate function
 void receive::terminate()
 {
   // (no terminate code required)
-}
-
-// Root inport: '<Root>/packet' set method
-void receive::setpacket(uint16_t localArgInput)
-{
-  receive_U.packet = localArgInput;
-}
-
-// Root outport: '<Root>/data' get method
-uint8_t receive::getdata() const
-{
-  return receive_Y.data;
-}
-
-// Root outport: '<Root>/ACK' get method
-uint8_t receive::getACK() const
-{
-  return receive_Y.ACK;
 }
 
 const char* receive::RT_MODEL_receive_T::getErrorStatus() const
