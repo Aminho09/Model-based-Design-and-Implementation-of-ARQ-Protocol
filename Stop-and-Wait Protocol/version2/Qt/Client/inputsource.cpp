26b7927f61e825bb9fd00c50bc301bca0039ad5b
Amin Ehsanimehr <81440400+Aminho09@users.noreply.github.com>
2025-03-03 13:36:45 +0330

    Delete Stop-and-Wait Protocol/version2 directory

--- Stop-and-Wait Protocol/version2/Qt/Client/inputsource.cpp
+++
@@ -1,9 +0,0 @@
-#include "inputsource.h"
-
-InputSource::InputSource(QObject *parent) : QObject(parent){}
-
-void InputSource::generateInput(const QString &sender, quint16 senderPort, uint8_t ACK) {
-    qDebug() << qPrintable(QString("Received from %1:%2 - %3").arg(sender).arg(senderPort).arg(ACK));
-    emit inputReady(ACK);
-}
-
--- Stop-and-Wait Protocol/version2/Qt/Server/outputsink.cpp
+++
@@ -1,9 +0,0 @@
-#include "outputsink.h"
-
-OutputSink::OutputSink(QObject *parent) : QObject(parent){}
-
-void OutputSink::processOutput(uint8_t data, uint8_t ACK) {
-
-    qDebug() << "Data validated, int:" << data << "char:" << (char)data;
-    emit sendtoUDP(ACK);
-}
